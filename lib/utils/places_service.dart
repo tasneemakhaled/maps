4464b3c2eec962734f66fcdd23ad2cd37a7f1789
tasneemakhaled <khaledtasneem402@gmail.com>
2026-01-10 12:06:45 +0200

    places autocomolete service done

--- lib/utils/places_service.dart
+++ lib/utils/places_service.dart
@@ -1,1 +1,21 @@
+import 'dart:convert';
 
+import 'package:http/http.dart' as http;
+import 'package:maps/models/places_autocomplete_model/places_autocomplete_model.dart';
+
+class PlacesService {
+    Future<List<PlacesAutocompleteModel>> getPredictions({required String input})async{
+    var response =await http.get(Uri.parse('https://photon.komoot.io/api/?q=$input')) ;
+    if (response.statusCode==200){
+        var data =jsonDecode(response.body)['features'];
+        List<PlacesAutocompleteModel>places=[];
+        for (var item in data) {
+          places.add(PlacesAutocompleteModel.fromJson(item));
+        }
+        return places;
+    }
+    else{
+        throw Exception();
+    }
+    }
+}
